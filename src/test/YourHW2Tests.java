ca01fbb889e33f0eb0bcaa15753c449fa88e575b
Harrison Lu <harrisonlu@wustl.edu>
2019-02-10 23:50:54 -0600

    asdf

--- src/test/YourHW2Tests.java
+++ src/test/YourHW2Tests.java
@@ -12,8 +12,14 @@ import org.junit.Test;
 
 import hw1.Catalog;
 import hw1.Database;
+import hw1.Field;
 import hw1.HeapFile;
+import hw1.IntField;
+import hw1.StringField;
+import hw1.Tuple;
 import hw1.TupleDesc;
+import hw1.Type;
+import hw2.Relation;
 
 public class YourHW2Tests {
 
@@ -50,8 +56,25 @@ public class YourHW2Tests {
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRelationToString() {
+		Relation ar = new Relation(ahf.getAllTuples(), atd);
+		String s = ar.toString();
+		String tupleDescString = ar.getDesc().toString();
+		assertTrue("TupleDesc toString does not contain tupleDesc", s.contains(tupleDescString));
+		for (int i = 0; i < ar.getTuples().size(); i++) {
+			for (int j = 0; j < ar.getDesc().getSize(); j++) {
+				Type type = ar.getDesc().getType(j);
+				Field field = ar.getTuples().get(i).getField(j);
+				if (type == Type.STRING) {
+					StringField newString = new StringField(field.toByteArray());
+					assertTrue("Relation does not contain string " + newString.toString(), s.contains(newString.toString()));
+				}
+				else if (type == Type.INT) {
+					IntField newInt = new IntField(field.toByteArray());
+					assertTrue("Relation does not contain int " + newInt.toString(), s.contains(newInt.toString()));
+				}
+			}
+		}
 	}
 
 }
