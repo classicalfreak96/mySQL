6c2408e230c0ee297aee060866227170ac95952e
HuangDavid <d.huang@wustl.edu>
2019-03-28 15:36:17 -0500

    wrote one test

--- src/test/YourHW4Tests.java
+++ src/test/YourHW4Tests.java
@@ -2,13 +2,58 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import hw1.Catalog;
+import hw1.Database;
+import hw1.HeapFile;
+import hw1.TupleDesc;
+import hw4.BufferPool;
+import hw4.Permissions;
+
 public class YourHW4Tests {
+	
+	private Catalog c;
+	private BufferPool bp;
+	private HeapFile hf;
+	private TupleDesc td;
+	private int tid;
+	
+	@Before
+	public void setup() {
+		
+		try {
+			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println("unable to copy files");
+			e.printStackTrace();
+		}
+		
+		c = Database.getCatalog();
+		c.loadSchema("testfiles/test.txt");
+		
+		int tableId = c.getTableId("test");
+		td = c.getTupleDesc(tableId);
+		hf = c.getDbFile(tableId);
+		
+		bp = Database.getBufferPool();
+		
+		tid = c.getTableId("test");
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLock() throws Exception {
+	    bp.getPage(0, tid, 0, Permissions.READ_WRITE);
+	    bp.getPage(0, tid, 0, Permissions.READ_WRITE);
+	    assertTrue(bp.holdsLock(0, tid, 0) == true);
+	    bp.transactionComplete(0, true);
+	    assertTrue(bp.holdsLock(0, tid, 0) == false);
 	}
 
 }
